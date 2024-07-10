c97c44bbaecb0462385bd34be9a1f6eb88a85269
Davichobits <davidruizdeveloper@gmail.com>
2024-07-09 19:32:42 -0500

    Infrastructure created

---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,20 @@
+import 'package:cinemapedia_app/config/constants/enviroment.dart';
+import 'package:cinemapedia_app/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MovieDBDatasource extends MoviesDatasource {
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Enviroment.theMovieDBKey,
+    'language': 'es-MX',
+  }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
