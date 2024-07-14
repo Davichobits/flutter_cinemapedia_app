3a6fb9ec2a16955c46c4907449a35990d0da1417
Davichobits <davidruizdeveloper@gmail.com>
2024-07-14 04:51:37 -0500

    Movies Provider

--- lib/presentation/providers/movies_repository_provider.dart
+++ lib/presentation/providers/movies_repository_provider.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia_app/infrastructure/datasources/moviedb_datasource.da
 import 'package:cinemapedia_app/infrastructure/repositories/movie_repository_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// Este repositorio es inmutable
 final movieRepositoryProvider = Provider((ref) {
   return MovieRepositoryImpl(MovieDBDatasource());
 });
