ba5049e09d78f7718f4199525ee6a7761baba4c8
Davichobits <davidruizdeveloper@gmail.com>
2024-07-23 12:25:28 -0500

    Showing movies with title, subtitle and rating

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia_app/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -22,8 +23,85 @@ class MoviesHorizontalListview extends StatelessWidget {
         _Text(
           title: title,
           subtitle: subtitle,
+        ),
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return _Slide(movie);
+            },
+          ),
         )
       ]),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Poster
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                height: 210,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8), 
+                      child: Center(child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 5),
+
+          // Title
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyles.titleSmall,
+            ),
+          ),
+
+          // Rating
+          Row(
+            children: [
+              Icon(Icons.star_half_outlined, size: 20, color: Colors.yellow.shade800),
+              const SizedBox(width: 3),
+              Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+              const SizedBox(width: 10),
+              Text('${movie.voteCount}', style: textStyles.bodySmall?.copyWith(color: Colors.yellow.shade800)),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
